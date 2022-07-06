6b8e3f7085fa6f060ba84035f43668f6d1cb3178
Jawad Mohammed <80121274+jawad-mohammed@users.noreply.github.com>
2022-07-06 17:53:19 +0530

    Update route.js

--- route.js
+++ route.js
@@ -7,7 +7,8 @@ const jwtAuthorize = require('./utils/jwtAuthorize')
 
 const pool = require('./db')
 
-router.post('/register',(req,res)=>{
+router.post('/register',async(req,res)=>{
+
     const registerUser = await pool.query(`insert into lmvcustomerform(
         emp_id,emp_name,mobile,email,alternate_mobile,pan_card,adhar_card,status,bank_details,bank_name,
         account_no,ifsc_code,branch_name,address_one,address_two,pin_code,state,district,city,company,
@@ -44,10 +45,48 @@ router.post('/register',(req,res)=>{
 })
 
 
+//login route
+router.post('/login',async(req,res)=>{
+
+// user must be stored in database
+const checkUser = await pool.query(`select * from users where mobile =$1`,[req.body.mobile])
+if(checkUser.rows.length === 0) return res.json(`user not exists`)
+
+    //get the user mobile  and verify in database comapre user mobile no with database mobile no
+    const validPassword = await bcrypt.compare(req.body.mobile,checkUser.rows[0].mobile)
+
+  //it verifies the otp with node generated otp
+//    const val = Math.floor(1000 + Math.random() * 9000);
+//     console.log(val);
+//      res.json(val)
+const verifyToken = (req.body.token=== 123)
+if(!verifyToken) return res.json('its false token')
+
+     if(!validPassword) return res.json(`invalid password`)
+    if(!verifyToken) return res.json('invalid token') 
+    
+        const jwtToken = jwtHelper(checkUser.rows[0].userid)
+   return res.json({myJwtToken:jwtToken})
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
 router.get('/get', async(req,res)=>{
     const getAlUsers = await pool.query(`select * from lmvcustomerform`)
     res.json(getAlUsers.rows)
 })
+
+
 
 
 
